fc4f5066b9809f2c7e1a434eab8c90b871f60bd9
Laith <laithk1@hotmail.co.uk>
2019-10-10 06:10:03 +0100

    Updte index.js
    Another update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Dockerfile test World!");
+    response.end("Hello Dockerfile test World! and another update");
 });
 
 const port = process.env.PORT || 1337;
